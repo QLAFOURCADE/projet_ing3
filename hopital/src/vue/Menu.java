102dca13ff973bda4b762f24e1fe45a067994e40
DESKTOP-ER9EDGF\Tiphaine Plusquellec <tiphaineplusquellec@sfr.fr>
2018-04-18 10:08:00 +0200

    InterfaceMenu plus fonctions
    Panels pour chaque classe et les boutons .

--- hopital/src/vue/Menu.java
+++ hopital/src/vue/Menu.java
@@ -6,93 +6,153 @@
 package vue;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.GridLayout;
 import static java.awt.SystemColor.menu;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.Box;
 import static javax.swing.Box.createVerticalBox;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
  *
  * @author Tiphaine Plusquellec
  */
-public class Menu extends JFrame {
+public class Menu extends JFrame implements ActionListener {
 
+        // Les boutons
     private JButton bouton = new JButton("ESPACE ADMINISTRATIF DE L'HOPITAL");
     private JButton bouton1 = new JButton("Quitter");
-    private JButton bg;
-    private JButton bg1;
-    private JButton bg2;
-    private JButton bg3;
-    private JButton bg4;
-    private JButton bg5;
-    private JButton bg6;
-    private JButton bg7;
-    
+    private ArrayList<JButton> list_button;
+    private ArrayList<JPanel> list_Panel;
+    private ArrayList<JButton> list_bouton_docteur;
+   
+    private JButton Ajout;
+    // Les panels
     private JPanel gestion;
     private JPanel grille;
     private JPanel affichage;
-    private JButton Recherche;
-    private JButton Ajout_Suppr;
-
-    public Menu() {
+    private JPanel formulaire;
+    private JPanel card;
+    private CardLayout cl;
+    public Menu(){
         this.setTitle("ESPACE ADMINISTRATIF DE L'HOPITAL");
         this.setSize(1200, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setLayout(new BorderLayout());
-        bg= new JButton("Docteur");
-        bg1= new JButton("Infirmier");
-        bg2= new JButton("Service");
-        bg3= new JButton("Employee");
-        bg4= new JButton("Hospitalisation");
-        bg5= new JButton("Soigne");
-        bg6= new JButton("Chambre");
-        Recherche = new JButton("Recherche");
-        Ajout_Suppr= new JButton("Ajout_Suppr");
+        // instantion des boutons
+        grille = new JPanel(new GridLayout(0, 1));
+        list_button=new ArrayList<>();
+        list_button.add(new JButton("Docteur"));
+        list_button.add(new JButton("Employée"));
+        list_button.add(new JButton("Malade"));
+        list_button.add(new JButton("Infirmier"));
+        list_button.add(new JButton("Chambre"));
+        
+        // instanier les boutons du docteurs
+        list_bouton_docteur= new ArrayList<>();
+        list_bouton_docteur.add(new JButton("numero"));
+        list_bouton_docteur.add(new JButton("nom"));
+        list_bouton_docteur.add(new JButton("prenom"));
+        list_bouton_docteur.add(new JButton("adresse"));
+        list_bouton_docteur.add(new JButton("tel"));
+        list_bouton_docteur.add(new JButton("specialite"));
+        list_bouton_docteur.add(new JButton ("Recherche") );
+        
+        // Remplissage du Panel Grille des boutons et initialisation de la fonction ActionListner
+        
+        for (JButton jButton : list_button) {
+            grille.add(jButton);
+            jButton.addActionListener((ActionListener) this);
+        }
+        
+        // Initialisation des Panel de chaque classe
+        list_Panel=new ArrayList<>();
+        list_Panel.add(new JPanel(new GridLayout(0,1)));
+        list_Panel.add(new JPanel(new GridLayout(0,1)));
+        list_Panel.add(new JPanel(new GridLayout(0,1)));
+        list_Panel.add(new JPanel(new GridLayout(0,1)));
+        list_Panel.add(new JPanel(new GridLayout(0,1)));
+        list_Panel.add(new JPanel(new GridLayout(0,1)));
+        list_Panel.add(new JPanel(new GridLayout(0,1)));
+        list_Panel.add(new JPanel(new GridLayout(0,1)));
+        cl = new CardLayout();
+        card=new JPanel(cl);
+        
+        for (JPanel jPanel : list_Panel) {
+            card.add(jPanel);
+        }
+        this.add(card);
+        
+        // remplissage du Panel(0) correspondant à Docteur
+        for (int i = 0; i < 10; i++) {
+            list_Panel.get(0).add(new JLabel("eeee"));
+            // Ajout des boutons dans le panel 0 correspondant à docteur
+            list_Panel.get(0).add(list_bouton_docteur.get(0));
+            list_Panel.get(0).add(list_bouton_docteur.get(1));
+            list_Panel.get(0).add(list_bouton_docteur.get(2));
+            list_Panel.get(0).add(list_bouton_docteur.get(3));
+            list_Panel.get(0).add(list_bouton_docteur.get(4));
+            list_Panel.get(0).add(list_bouton_docteur.get(5));
+            list_Panel.get(0).add(list_bouton_docteur.get(6));
+            // Ajout des fonctions Recherche et trier dans les boutons
+
+        }
+        list_Panel.get(1).setBackground(Color.blue);
+
+
+       //
+        Ajout= new JButton("Ajout");
+        
+        affichage= new JPanel();
+        formulaire= new JPanel();
         
         
+        // Premhopiier Panel Menu
         gestion = new JPanel(new GridLayout(0, 1));
         gestion.setBackground(Color.BLUE);
-        gestion.add(Recherche);
-        gestion.add(Ajout_Suppr);
+        gestion.add(Ajout);
         
-        grille = new JPanel(new GridLayout(0, 1));
+        
         grille.setBackground(Color.RED);
-        grille.add(bg);
-        grille.add(bg1);
-        grille.add(bg2);
-        grille.add(bg3);
-        grille.add(bg4);
-        grille.add(bg5);
-        grille.add(bg6);
+        
         affichage = new JPanel(new GridLayout(0, 1));
         affichage.setBackground(Color.GREEN);
 
-        this.add(affichage, BorderLayout.EAST);
-        this.add(grille, BorderLayout.CENTER);
+        this.add(grille, BorderLayout.EAST);
         this.add(gestion, BorderLayout.WEST);
 
-        // Disposition des boutons en grille
-        //this.setLayout(new GridLayout(3, 2));
-        //GridLayout gl = new GridLayout();
-        //gl.setColumns(2);
-        //gl.setRows(3);
-        //this.setLayout(gl);
-        /*Box b1 = new createVerticalBox();
-        b1.add(new JButton("Recherche"));
-        b1.add(new JButton("Ajouter/Supprimer"));
-
-        //On ajoute le bouton au content pane de la JFrame
-        this.getContentPane().add(bouton, BorderLayout.CENTER);
-        this.getContentPane().add(bouton1, BorderLayout.SOUTH);*/
+        
         this.pack();
         this.setVisible(true);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(list_button.contains(e.getSource())) {
+            
+        for(int i=0; i< list_Panel.size(); i++){
+            
+        list_Panel.get(i).setVisible((list_button.get(i).equals(e.getSource())));
+        }
+        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+
+
 }
+    
+        
+        
+    
+
+
